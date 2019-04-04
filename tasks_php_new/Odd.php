b92279f204cd85e98a1e1dd4766608d5a134d232
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2019-04-04 13:16:52 +0200

    Let's remove the comment otherwise surround with works incorrectly and requires additional space

--- tasks_php_new/Odd.php
+++ tasks_php_new/Odd.php
@@ -3,5 +3,5 @@
 $tasks = array(1, /*caret*/2, 3, 4, 5, 6, 7, 8, 9, 10);
 
 foreach ($tasks as $task) {
-    echo $task . "\n"; // print only odd numbers
+    echo $task . "\n";
 }
--- tasks_php_new/Odd_.php
+++ tasks_php_new/Odd_.php
@@ -4,6 +4,6 @@ $tasks = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
 foreach ($tasks as $task) {
     if ($task % 2 == 0) {
-        echo $task . "\n"; // print only odd numbers
+        echo $task . "\n";
     }
 }
